daf3093e1e9f8b5fbcefcc9b758f9c0534a2021c
BeagleXV <beagle@gmail.com>
2023-09-28 14:08:47 -0300

    hello, git world!

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 80){ // condição simples
+    console.log(`Vc ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
